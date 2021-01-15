85e56ccd8722da249c73cad8b55b1ffb4a952e10
hannesschaletzky <hannes.schaletzky@gmx.de>
2021-01-15 14:45:02 +0100

    Reload Form1

--- twitter_oAuthParams_Generator/Form1.cs
+++ twitter_oAuthParams_Generator/Form1.cs
@@ -10,8 +10,53 @@ using System.Windows.Forms;
 
 namespace twitter_oAuthParams_Generator {
     public partial class Form1 : Form {
+        
         public Form1() {
             InitializeComponent();
+            start();
+        }
+
+        private void start() {
+
+            Console.WriteLine("Program started");
+
+            //oauth_timestamp
+            var oauth_timestamp = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+            Console.WriteLine($"oauth_timestamp: {oauth_timestamp}");
+
+            //oauth_nonce
+            string oauth_nonce = randomString(32);
+            Console.WriteLine($"oauth_nonce: {oauth_nonce}");
+
+            //signature
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// retrieve random alphanumeric string for oauth_nonce
+        /// </summary>
+        private static Random random = new Random();
+        public static string randomString(int length) {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
